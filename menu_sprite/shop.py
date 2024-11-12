6cf3181eaa1f2f37430ffa064c1880a09cea5873
Paul-coder72 <p.leroux.72510@gmail.com>
2024-11-12 11:07:05 +0100

    Update shop.py

---
+++ menu_sprite/shop.py
@@ -0,0 +1,7 @@
+import pygame
+
+shop_shelf = pygame.image.load('assets/img/icons/shop/shop_shelf.png')
+shop_shelf_pos = shop_shelf.get_rect()
+shop_shelf_pos.center = (512, 370)
+
+shop_bg = pygame.image.load('assets/img/icons/shop/bg_shop.png')
