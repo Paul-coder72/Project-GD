2fe94177dbe11590793078028d79d88617344254
Paul-coder72 <p.leroux.72510@gmail.com>
2024-11-16 10:27:53 +0100

    Update variables.py

--- utils/variables.py
+++ utils/variables.py
@@ -1,11 +1,17 @@
 import pygame
+from utils.load_bg import *
 
+icon = pygame.image.load("assets/icon.ico")
+pygame.display.set_caption("Geometry Dash")
+pygame.display.set_icon(icon)
+
+
+window = pygame.display.set_mode((1024, 560))
+
+music = -1
+
+wrect = window.get_rect()
+
+background = bg7.convert()
 
 clock = pygame.time.Clock()
-
-
-lvl1 = False
-lvl2 = False
-lvl3 = False
-lvl4 = False
-lvl5 = False
